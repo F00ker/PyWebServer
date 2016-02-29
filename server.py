547c1527686d5ad11c16ee4aa2b1f41a2d692146
James Allardice <james.allardice@fr.clara.net>
2016-02-29 16:02:35 +0100

    refactoring broke everything...

--- server.py
+++ server.py
@@ -15,10 +15,13 @@ class http_listener:
         if "GET" in self.request_buffer:
             GET_request = self.request_buffer.split("\n", 1)
             request_PATH = GET_request[0].split(" ", 2)
+            print request_PATH
             return request_PATH
 
         if "HEAD" in self.request_buffer:
             return [0,"/curious"]
+
+        print "end get_request_path"
 
     def get_request_type(self,request):
         self.request_type = request.split()[0]
@@ -32,7 +35,7 @@ class http_listener:
     def get_content(self,request):
         try:
             self.response_buffer = ""
-            print os.getcwd()+get_request_path(request)
+            #print os.getcwd()+get_request_path(request)
             with open(os.getcwd()+request_PATH[1], 'r') as requested_file:
                 for line in requested_file:
                     print line
@@ -48,10 +51,10 @@ class http_listener:
         self.serversocket.listen(5)
         while True:
             self.connection, address = self.serversocket.accept()
-            request_buffer = connection.recv(4096)
+            request_buffer = self.connection.recv(4096)
             if len(request_buffer) > 0:
                 try:
-                    print request_buffer
+                    #print request_buffer
                     self.connection.sendall(get_content(self.get_request_path(request_buffer)))
                     self.connection.close()
                 except Exception as e:
